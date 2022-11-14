1343099346034cbbda638cfe9dee5e35a2972ed9
Matthias Besnier <besnier.matthias@gmail.com>
2022-11-14 10:04:49 +0100

    Update script_data.py

--- script_data.py
+++ script_data.py
@@ -147,10 +147,3 @@ if __name__ == "__main__":
     #print(generate_users())
     #print(generate_bleat())
     #print(generate_relationship())
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-
-    api = tweepy.API(auth)
-
-    public_tweets = api.home_timeline()
-    for tweet in public_tweets:
-        print(tweet.text)
