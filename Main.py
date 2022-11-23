97653532293f50a1c88dc044bfe392538e1e8f0d
Matthias Besnier <besnier.matthias@gmail.com>
2022-11-23 22:58:22 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -575,6 +575,6 @@ if __name__ == "__main__":
 
     with app.app_context():
         db.create_all()
-    app.debug = True
+    app.debug = False
     app.env = "development"
     app.run(host="localhost", port="5000")
